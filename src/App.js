78338b5532b22655718f0768e5ff693340907ff2
Bhanu Prakash K <bhanuprakashinter12@gmail.com>
2023-10-04 04:49:14 +0000

    Searchable List

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import {Component} from 'react'
+import UserProfile from './components/UserProfile'
+
+import './App.css'
+
+const intialUserDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Esther Howard',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Floyd Miles',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Jacob Jones',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Devon Lane',
+    role: 'Software Developer',
+  },
+]
+
+class App extends Component {
+  state = {
+    searchInput: '',
+    userDetailsList: intialUserDetailsList,
+  }
+
+  onSearchChangeInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
+  }
+
+  onDeleteUser = uniqueNo => {
+    const {userDetailsList} = this.state
+    const filteredDetailsList = userDetailsList.filter(
+      eachUser => eachUser.uniqueNo !== uniqueNo,
+    )
+    this.setState({
+      userDetailsList: filteredDetailsList,
+    })
+  }
+
+  render() {
+    const {searchInput, userDetailsList} = this.state
+    const searchResults = userDetailsList.filter(eachUser =>
+      eachUser.name.includes(searchInput),
+    )
+    return (
+      <div className="app-container">
+        <h1 className="title">Users List</h1>
+        <input
+          type="search"
+          value={searchInput}
+          onChange={this.onSearchChangeInput}
+        />
+        <ul className="list-container">
+          {searchResults.map(eachUser => (
+            <UserProfile
+              userDetails={eachUser}
+              key={eachUser.uniqueNo}
+              onDeleteUser={this.onDeleteUser}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
